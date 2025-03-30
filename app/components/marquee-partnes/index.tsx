7355f427dba5dbdb4ba336e66bea582b1f723e5a
Nathan Mota <nathansmota@gmail.com>
2025-03-30 19:00:34 -0300

    :bug: fix: ajusting marquee

--- app/components/marquee-partnes/index.tsx
+++ app/components/marquee-partnes/index.tsx
@@ -1,15 +1,15 @@
-import { Marquee } from "@/components/ui/marquee"
+import { Marquee } from "@/components/magicui/marquee"
 import Image from "next/image";
 
 const Logos = {
     ufu: () => (
-        <Image src="/partnes/ufu-logo.png" width={300} height={300} alt="Logo UFU" />
+        <Image src="/partnes/ufu-logo.png" width={200} height={200} alt="Logo UFU" />
     ),
     facom: () => (
-        <Image src="/partnes/facom-logo.png" width={350} height={350} alt="Logo FACOM" />
+        <Image src="/partnes/facom-logo.png" width={250} height={250} alt="Logo FACOM" />
     ),
     proex: () => (
-        <Image src="/partnes/proex-logo.png" width={300} height={300} alt="Logo Proex" />
+        <Image src="/partnes/proex-logo.png" width={200} height={200} alt="Logo Proex" />
     )
 };
 
@@ -18,15 +18,20 @@ export function MarqueeDemo() {
     const arr = [Logos.ufu, Logos.facom, Logos.proex]
 
     return (
-        <Marquee>
-            {arr.map((Logo, index) => (
-                <div
-                    key={index}
-                    className="relative h-full w-fit mx-[4rem] flex items-center justify-start"
-                >
-                    <Logo />
-                </div>
-            ))}
-        </Marquee>
+        <div className="pt-12 container mx-auto">
+            <div className="flex justify-center text-center">
+                <h1 className="text-3xl font-bold pb-12">Parceiros</h1>
+            </div>
+            <Marquee>
+                {arr.map((Logo, index) => (
+                    <div
+                        key={index}
+                        className="relative h-full w-fit mx-[4rem] flex items-center justify-start"
+                    >
+                        <Logo />
+                    </div>
+                ))}
+            </Marquee>
+        </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <AboutUs />
       <Projects />
       <Technologys />
-      {/* <MarqueeDemo /> */}
+      <MarqueeDemo />
     </div >
   );
 }
