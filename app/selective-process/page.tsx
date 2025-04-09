fa5dcfa95f58499f191f0cfff3eafa70fcd2f876
Nathan Mota <nathansmota@gmail.com>
2025-04-09 07:58:56 -0300

    :lipstick: updating page for follow standard

--- app/selective-process/page.tsx
+++ app/selective-process/page.tsx
@@ -5,7 +5,11 @@ import SelectionForm from './form';
 
 export default function SelectiveProcess() {
     return (
-        <div className="container mx-auto px-6 md:px-8 pt-20">
+        <div className="container mx-auto px-4 md:px-10 py-12">
+            <div className="text-center mb-16">
+                <h1 className="text-4xl font-gesrics md:text-5xl font-bold mb-4">Proecesso Seletivo</h1>
+                <div className="w-24 h-1 bg-yellow-500 mx-auto mb-6"></div>                
+            </div>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center p-8 rounded-2xl">
                 <motion.img
                     src="/arduino.png"
