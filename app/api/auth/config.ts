1caa038e909f20057bd3502fbf25be0c9125f783
Nathan Mota <nathansmota@gmail.com>
2025-04-16 23:54:14 -0300

    🚀 chore: disable ts for build

--- app/api/auth/config.ts
+++ app/api/auth/config.ts
@@ -2,29 +2,29 @@ import GitHubProvider from 'next-auth/providers/github'
 import { NextAuthOptions } from 'next-auth'
 
 export const authOptions: NextAuthOptions = {
-    providers: [
-        GitHubProvider({
-            clientId: process.env.GITHUB_CLIENT_ID!,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-            profile(profile) {
-                return {
-                    id: profile.id,
-                    name: profile.name,
-                    email: profile.email,
-                    image: profile.avatar_url,
-                    login: profile.login,
-                }
-            },
-        }),
-    ],
-    callbacks: {
-        async signIn({ user }) {
-            const allowedUsers = ['nathanmota-dev', 'Leonardo-781', 'brlacerra']
-            // @ts-ignore
-            return allowedUsers.includes(user.login)
-        },
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+      profile(profile) {
+        return {
+          id: profile.id,
+          name: profile.name,
+          email: profile.email,
+          image: profile.avatar_url,
+          login: profile.login,
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async signIn({ user }) {
+      const allowedUsers = ['nathanmota-dev', 'Leonardo-781', 'brlacerra']
+      // @ts-ignore – `login` só existe no perfil do GitHub
+      return allowedUsers.includes(user.login)
     },
-    pages: {
-        error: '/unauthorized',
-    },
+  },
+  pages: {
+    error: '/unauthorized',
+  },
 }
